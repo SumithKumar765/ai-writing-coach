c44ae60a9ce79f6d3b8931e127e75745adc53f21
sumith <kumarsumith98765@gmail.com>
2026-03-15 17:16:29 +0530

    solved cors errror

--- backend/main.py
+++ backend/main.py
@@ -179,7 +179,7 @@ app = FastAPI()
 # Allow the React frontend to communicate with this backend securely
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"], 
+    allow_origins=["*"], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
